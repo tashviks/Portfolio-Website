29a55f4919dceeff03c31b91275dc0659e344c88
tashviks <tashvik.12112279@lpu.in>
2023-06-25 23:43:59 +0530

    inital

---
+++ script.js
@@ -0,0 +1,25 @@
+// Ripple effect 
+$('#home').ripples({
+	resolution: 512,
+	dropRadius: 20,
+	perturbance: 0.04,
+});
+$('#about').ripples({
+	resolution: 512,
+	dropRadius: 20,
+	perturbance: 0.04,
+});
+//-----------------------------------------------------------------------------------------
+var typed = new Typed("#auto-type" , {
+		strings : ["Hola!" , "I am Tashvik" , "I'm a Student," , "I'm a Coder &" , "I'm a Web Developer" , "And this is my Portfolio"],
+		typeSpeed : 150,
+		backSpeed : 150,
+		loop : true
+	})
+
+//---------------------------------------------------
+
+ 
+ 
+ 	
+
